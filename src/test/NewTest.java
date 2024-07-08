8bb3dc4fa2884df9e09e783b98100340c78ad97a
Shruti <agrawal.shruti.a@gmail.com>
2024-07-08 11:10:57 +0530

    for develop branch architect

--- src/test/NewTest.java
+++ src/test/NewTest.java
@@ -8,13 +8,24 @@ public class NewTest {
   @Test(groups = {"Smoke"})
   public void f() {
 	  System.out.println("hello");
+	  System.out.println("hello1");
+	  System.out.println("hello");
+	  
+	  System.out.println("hello3");
+	  System.out.println("hello2");
+	  
+	  System.out.println("hello4");
   }
   @Test
   public void f1() {
-	  System.out.println("hello1");
+	  System.out.println("hellof1");
   } 
   @AfterSuite
   public void last() {
 	  System.out.println("I will execute last");
   }
+  @Test
+  public void f2() {
+	  System.out.println("hellof2");
+  } 
 }
